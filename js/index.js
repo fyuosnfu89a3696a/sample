b4802e99ec6373b4766c72dcf2faf2bf69c352d2
poppo <20new01777@gmail.com>
2024-03-23 18:50:33 +0900

    現在地取得

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 //(1) ページ本体が読み込まれたタイミングで実行するコード
 const result = document.getElementById("result");
-result.textContent = "Hello.JavaScript!!";
+if (navigator.geolocation) {
+    //(1)位置情報取得処理
+    navigator.geolocation.getCurrentPosition(
+        (pos) => {
+            //(2)位置情報取得成功時の処理
+            msg = `緯度:${pos.coords.latitude}<br>
+                経度:${pos.coords.longitude}<br>
+                方角:${pos.coords.heading}`;
+            result.innerHTML = msg;
+        }
+    );
+} else {
+    window.alert("geolocation APIに対応しているブラウザーでアクセスしてください。")
+}
 window.alert(result);
