57091d0f476f4ac8a084aa37dea1c7aee97c2466
poppo <20new01777@gmail.com>
2024-03-23 19:18:45 +0900

    エラー処理

--- js/index.js
+++ js/index.js
@@ -1,14 +1,26 @@
 //(1) ページ本体が読み込まれたタイミングで実行するコード
 const result = document.getElementById("result");
 if (navigator.geolocation) {
-    //(1)位置情報取得処理
     navigator.geolocation.getCurrentPosition(
         (pos) => {
-            //(2)位置情報取得成功時の処理
             msg = `緯度:${pos.coords.latitude}<br>
                 経度:${pos.coords.longitude}<br>
                 方角:${pos.coords.heading}`;
             result.innerHTML = msg;
+        },
+        (err) => {
+            const msgs = [
+                " ",
+                "Geolocationの利用が許可されていません。",
+                "位置情報を取得できません。",
+                "位置情報の取得中にタイムアウトしました。"
+            ];
+            result.textContent = msgs[err.code];
+        },
+        {
+            timeout : 7000,
+            maximumAge : 500,
+            endbleHighAccuracy: false
         }
     );
 } else {
